adcd87f590b3c21a901365fb4a29bcb283e0dc47
Borja Arias <borja.arias.upc@gmail.com>
2018-07-16 22:43:00 +0200

    Fix ssh connection on deploy 2

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -4,6 +4,5 @@ ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
 cd Projects/hathsin
 git fetch && \
 git reset --hard origin/HEAD && \
-docker-compose build --no-cache && \
-docker-compose up -d && \
+docker-compose up -d --build;
 exit"
