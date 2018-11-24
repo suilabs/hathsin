f68bdf2447607d63f31bfee7a3a92088f0faa064
Borja Arias <borja.arias.upc@gmail.com>
2018-11-24 19:54:27 +0100

    Update updateRepo script v3

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=hathsinV3
-ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${PROJECT_NAME}; exit 0"
+echo ${PWD}
+ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${PROJECT_NAME}; exit 0";
 rsync -rpulz --verbose ${PWD}/* /tmp/${PROJECT_NAME}/.
 LC_PROJECT_NAME=${PROJECT_NAME} ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
 cd Projects/${LC_PROJECT_NAME}
