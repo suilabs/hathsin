7457eae2e7d346c07b7265f9a9c9718ebe7711f9
Borja Arias <borja.arias.upc@gmail.com>
2018-11-24 19:56:34 +0100

    Update updateRepo script v4

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=hathsinV3
-echo ${PWD}
 ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${PROJECT_NAME}; exit 0";
-rsync -rpulz --verbose ${PWD}/* /tmp/${PROJECT_NAME}/.
-LC_PROJECT_NAME=${PROJECT_NAME} ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
+rsync -rpulz --verbose ${PWD}/../* /tmp/${PROJECT_NAME}/.
+export LC_PROJECT_NAME=${PROJECT_NAME}
+ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
 cd Projects/${LC_PROJECT_NAME}
 docker-compose up -d hathsin-database && \
 docker-compose up -d --build hathsin;
