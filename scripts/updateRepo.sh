ab6dd9e4163f0137d79bf3864446b9e15c6b2cb4
Borja Arias <borja.arias.upc@gmail.com>
2018-11-24 19:29:36 +0100

    remove docker dependency

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -4,5 +4,6 @@ ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
 cd Projects/hathsinV2
 git fetch && \
 git reset --hard origin/HEAD && \
-docker-compose up -d --build;
+docker-compose up -d hathsin-database && \
+docker-compose up -d --build hathsin;
 exit"
