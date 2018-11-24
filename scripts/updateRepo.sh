172f04230c45169349162781da8ea0fe36734014
Borja Arias <borja.arias.upc@gmail.com>
2018-11-24 20:10:45 +0100

    Add rsync

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=hathsinV3
-ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${PROJECT_NAME}; exit 0";
-rsync -rpulz --verbose ${PWD}/../* /tmp/${PROJECT_NAME}/.
-export LC_PROJECT_NAME=${PROJECT_NAME}
+ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${PROJECT_NAME}; exit" && \
+rsync -rpulz --verbose ${PWD}/../* /tmp/${PROJECT_NAME}/. && \
+export LC_PROJECT_NAME=${PROJECT_NAME} && \
 ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
 cd Projects/${LC_PROJECT_NAME}
 docker-compose up -d hathsin-database && \
