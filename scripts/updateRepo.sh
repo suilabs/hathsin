7ed599e0aa17d6a9969de243e27c91dde92464b7
Borja Arias <borja.arias.upc@gmail.com>
2018-11-24 20:14:35 +0100

    Fix typo rsync

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -2,7 +2,7 @@
 
 PROJECT_NAME=hathsinV3
 ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${PROJECT_NAME}; exit" && \
-rsync -rpulz --verbose ${PWD}/../* /tmp/${PROJECT_NAME}/. && \
+rsync -rpulz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${PROJECT_NAME}/. && \
 export LC_PROJECT_NAME=${PROJECT_NAME} && \
 ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
 cd Projects/${LC_PROJECT_NAME}
