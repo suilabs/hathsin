1b692b56f8d2f007805115e6d05b5f2542cf756f
Borja Arias <borja.arias.upc@gmail.com>
2018-11-25 20:45:37 +0100

    Typo in user password variables

--- service/src/config/serverConfig.js
+++ service/src/config/serverConfig.js
@@ -3,8 +3,8 @@ module.exports = {
   DB_SERVER: (process.env.DB_SERVER || 'localhost'),
   DB_PORT: (process.env.DB_PORT || 27017),
   DB_SCHEMA: (process.env.DB_SCHEMA || 'hathsin'),
-  DB_USER: (process.env.LC_MONGO_USERNAME),
-  DB_PASSWORD: (process.env.LC_MONGO_PASSWORD),
+  DB_USER: (process.env.MONGO_USERNAME),
+  DB_PASSWORD: (process.env.MONGO_PASSWORD),
   REQUEST_LOGGING: (process.env.REQUEST_LOGGING || 'false'),
   LOGGING_ENVIRONMENT: (process.env.LOGGING_ENVIRONMENT || 'dev'),
 };
